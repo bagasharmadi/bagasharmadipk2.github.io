7e92c0a20c9ba9e0a17f7d0a183ffa28234e04c8
Bagas Harmadi <71679110+bagasharmadi@users.noreply.github.com>
2020-12-15 07:44:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 let nama = prompt("Masukkan Nama Anda :");
 document.getElementById("name").innerHTML = nama;
+function runPopup(){window.alert("Selamat Datang di Website Kami, Mohon Maaf kalau website nya jelek");};
